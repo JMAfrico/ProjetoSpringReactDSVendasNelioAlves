24977ef6d1111afa67673eedc46b1799a68b3ae1
JMAfrico <joao_marcossilva@hotmail.com>
2021-09-06 19:28:11 -0300

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,15 @@
+function Footer() {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por João Marcos</p>
+                <p className="text-light">GitHub <a href="https://github.com/JMAfrico" target="_blank" rel="noreferrer">João Marcos</a></p>
+                <p className="text-light">LinkedIn <a href="https://www.linkedin.com/in/jo%C3%A3o-marcos-africo-da-silva-070955143/" target="_blank" rel="noreferrer">João Marcos</a></p>
+                <p className="text-light"><small><strong>Semana Spring React</strong><br />
+                    Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
